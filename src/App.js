6dd35ad21724e96fd0921c0c703a9afb31225674
Bishal487 <bishal.kunwar487@gmail.com>
2023-11-17 18:47:18 -0500

    Setup for joke generating app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 // import Home from "../src/components/Home/Home";
 // import PaintHome from "./components/Paint_App/PaintHome";
-import Eraser from "./components/Paint_App/Eraser";
+// import Eraser from "./components/Paint_App/Eraser";
+import JokeHome from "./components/JokeGenerator/JokeHome";
 
 function App() {
   return (
     <div className="App">
-      <Eraser/> 
+      <JokeHome/>
     </div>
   );
 }
---
+++ src/components/JokeGenerator/JokeHome.js
@@ -0,0 +1,7 @@
+
+export default function JokeHome(){
+
+    return(
+        <p>Hello I ll generate a random joke for you , smaashhhhhh</p>
+    )
+}
