23a21f6b841477f3528ce20c1c3ddb1fad32693b
Bishal487 <bishal.kunwar487@gmail.com>
2023-11-21 11:45:14 -0500

    rendering json data

--- src/components/ShoppincCart/CartHome.js
+++ src/components/ShoppincCart/CartHome.js
@@ -1,16 +1,19 @@
 
-// import { useState } from "react";
+import { useState } from "react";
 import "./CartHome.css";
 import data from "./data.json" ;
+import ShowCourses from "./ShowCourses";
 
 export default function CartHome(){
 
     // const courses = useState(data);
     const dataRendered = data.map((data)=><ul><li key={data.id}>{data.name}</li></ul>)
+    const[courses, setCourses]=useState(dataRendered);
+
 
     return(
         <div className="cart-home">
-            <p>{dataRendered}</p>
+            <ShowCourses course={dataRendered}/>
         </div>
     );
 };
