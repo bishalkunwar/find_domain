dd38b699a6570a5cd248a26fc0cbcf38e22ca17d
Bishal487 <bishal.kunwar487@gmail.com>
2023-11-21 11:09:22 -0500

    movie trailer api via youtube api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // import Home from "../src/components/Home/Home";
 // import PaintHome from "./components/Paint_App/PaintHome";
 // import Eraser from "./components/Paint_App/Eraser";
-import JokeHome from "./components/JokeGenerator/JokeHome";
+// import JokeHome from "./components/JokeGenerator/JokeHome";
+
+import MovieApp from "./components/MovieApp/Movie";
 
 function App() {
   return (
     <div className="App">
-      <JokeHome/>
+      <MovieApp/>
     </div>
   );
 }
